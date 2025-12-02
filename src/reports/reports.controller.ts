8f9f928707f2c1612bbdc8c02d752f8849addf64
Oleg <luzeninoleg2012@gmail.com>
2025-12-02 21:59:17 +0200

    started repository

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private ReportsService: ReportsService) {}
+
   @Post()
-  createReport(@Body() body: CreateReportDto) {}
+  @UseGuards(AuthGuard)
+  async createReport(@Body() body: CreateReportDto) {
+    const report = await this.ReportsService.create(body);
+    return report;
+  }
 }
