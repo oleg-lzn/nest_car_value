1aa7e54a46eaa9dfa150eef36886e90c81d3626d
Oleg <luzeninoleg2012@gmail.com>
2025-12-05 11:37:20 +0200

    reports service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ReportsEntity } from './reports.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(ReportsEntity) private repo: Repository<ReportsEntity>,
+  ) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
+  }
+}
